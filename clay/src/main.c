3d810107199800ca58155d6d4d2d42d77cb63483
mattheww <matthew@Beas-iMac.local>
2022-06-16 18:14:55 +0100

    commit

--- clay/src/main.c
+++ clay/src/main.c
@@ -1,11 +1,15 @@
 #include "../include/clay.h"
 #include <stdio.h>
 
+int error_invalid_file = 247;
+
 int main(int argc, char* argv[])
 {
     if (argc < 2)
     {
         printf("[FATAL]: invalid file {'\\0'}\n");
+        printf("[error %i]\n", error_invalid_file);
+        return error_invalid_file;
     }
 
     return 0;
