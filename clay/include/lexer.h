e61d97eb31ee0b953f77d96620c9535bef055930
mattheww <matthew@Beas-iMac.local>
2022-06-16 17:42:06 +0100

    commit

--- clay/include/lexer.h
+++ clay/include/lexer.h
@@ -2,6 +2,7 @@
 #define CLAY_LEXER_H
 #include "token.h"
 #include <stdio.h>
+
 typedef struct LEXER_STRUCT
 {
     char* src;
@@ -13,6 +14,8 @@ typedef struct LEXER_STRUCT
 lexer_T* init_lexer(char* src);
 
 void lexer_advance(lexer_T* lexer);
+
+void lexer_skip_whitespace(lexer_T* lexer);
 
 token_T* lexer_next_token(lexer_T* lexer);
 #endif
--- clay/src/lexer.c
+++ clay/src/lexer.c
@@ -1,9 +1,11 @@
 #include "../include/lexer.h"
+#include <string.h>
 
 lexer_T* init_lexer(char* src)
 {
     lexer_T* lexer = calloc(1, sizeof(struct LEXER_STRUCT));
     lexer->src = src;
+    lexer->src_size = strlen(src);
     lexer->i = 0;
     lexer->c = src[lexer->i];
 
@@ -12,10 +14,27 @@ lexer_T* init_lexer(char* src)
 
 void lexer_advance(lexer_T* lexer)
 {
-
+    if (lexer->i < lexer->src_size && lexer->c != '\0')
+    {
+        lexer->i += 1;
+        lexer->c = lexer->src[lexer->i];
+    }
 }
 
-token_T* lexer_next_token(lexer_T* lexer);
+void lexer_skip_whitespace(lexer_T* lexer)
 {
+    while (lexer->c == 13 || lexer->c == 10 || lexer->c == ' ' || lexer->c == '\t')
+    {
+        lexer_advance(lexer);
+    }
+}
 
+token_T* lexer_next_token(lexer_T* lexer)
+{
+    while (lexer->c != '\0')
+    {
+        if (isalnum(lexer->c))
+            return lexer_advance_with(lexer_parse_id(lexer));
+        
+    }
 }
